4617d9c054233014a6455b662dffd7ee29ed7be4
Peterdinis <peterdinis0@pobox.sk>
2023-12-28 20:15:45 +0100

    feat: Github login WIP

--- src/app/components/auth/LoginComponent.tsx
+++ src/app/components/auth/LoginComponent.tsx
@@ -1,13 +1,29 @@
+"use client"
+
 import { FC } from "react";
 import Header from "../shared/Header";
 import { Button } from "@/components/ui/button";
+import { supabaseClient } from "@/supabase/setup";
+import { usePathname } from "next/navigation";
 
 const LoginComponent: FC = () => {
+    const pathName = usePathname();
+
+    const handleLoginGithub = async () => {
+        await supabaseClient.auth.signInWithOAuth({
+            provider: "github",
+            options: {
+                redirectTo: location.origin + "/auth/callback?next=" + pathName
+            }
+        })
+    }
+
+
     return (
         <>
             <Header text="Login" />
             <div className="flex justify-center align-top mt-5">
-                <Button variant={"default"}>
+                <Button onClick={handleLoginGithub} variant={"default"}>
                     Login here
                 </Button>
             </div>
