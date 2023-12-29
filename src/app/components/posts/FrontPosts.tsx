9c1f2f32c7b95f7cd1ee61cc7bbf40e528b73297
Peterdinis <peterdinis0@pobox.sk>
2023-12-29 10:23:07 +0100

    feat: Finish homepage design

--- src/app/components/posts/FrontPosts.tsx
+++ src/app/components/posts/FrontPosts.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import PostCard from './PostCad';
 
 const FrontPosts: FC = () => {
     return (
@@ -11,271 +12,52 @@ const FrontPosts: FC = () => {
             </h1>
             <div className='2xl:container 2xl:mx-auto flex flex-wrap items-start justify-center pt-6 gap-6'>
                 <div className='flex lg:flex-col sm:flex-row flex-col items-start lg:gap-0 gap-6 lg:w-96 w-auto'>
-                    <div className='hover:scale-125 transition-all duration-500 cursor-pointer'> {/* TODO: Animácia na celú kartu */}
-                        <img
-                            src='https://i.ibb.co/hKV7YhC/blog-1.png'
-                            alt='woman smiling'
-                        />
-                        <h2 className='text-xl font-semibold leading-5 mt-8 text-gray-800'>
-                            Busting myths about hair
-                        </h2>
-                        <div className='mt-6 flex items-center cursor-pointer'>
-                            <p className='pr-3 text-base font-medium leading-4 underline text-gray-800'>
-                                5 min read
-                            </p>
-                            <svg
-                                width={16}
-                                height={10}
-                                viewBox='0 0 16 10'
-                                fill='none'
-                                xmlns='http://www.w3.org/2000/svg'
-                            >
-                                <path
-                                    d='M1 5H15'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 9L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 1L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                            </svg>
-                        </div>
-                        <p className='text-base mt-4 italic font-italic leading-4 text-gray-600'>
-                            23, Feburary 2021
-                        </p>
-                    </div>
-                    <div className='lg:mt-10'>
-                        <img
-                            className='hover:scale-125 transition-all duration-500 cursor-pointer'
-                            src='https://i.ibb.co/WkCydhJ/blog-4.png'
-                            alt='coffe pouring'
-                        />
-                        <h2 className='text-xl font-semibold leading-5 mt-8 text-gray-800'>
-                            Busting myths about hair
-                        </h2>
-                        <div className='mt-6 flex items-center cursor-pointer'>
-                            <p className='pr-3 text-base font-medium leading-4 underline text-gray-800'>
-                                5 min read
-                            </p>
-                            <svg
-                                width={16}
-                                height={10}
-                                viewBox='0 0 16 10'
-                                fill='none'
-                                xmlns='http://www.w3.org/2000/svg'
-                            >
-                                <path
-                                    d='M1 5H15'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 9L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 1L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                            </svg>
-                        </div>
-                        <p className='text-base mt-4 italic font-italic leading-4 text-gray-600'>
-                            23, Feburary 2021
-                        </p>
-                    </div>
+                    <PostCard
+                        image={'https://picsum.photos/200/300'}
+                        imageAlt={'t'}
+                        header={'t'}
+                        timeRead={'3min'}
+                        postCreatedDate={'t'}
+                    />
+                    <PostCard
+                        image={'https://picsum.photos/200/300'}
+                        imageAlt={'t'}
+                        header={'t'}
+                        timeRead={'3min'}
+                        postCreatedDate={'t'}
+                    />
                 </div>
                 <div className='flex lg:flex-col sm:flex-row flex-col items-start lg:gap-0 gap-6 lg:w-96 w-auto'>
-                    <div>
-                        <img
-                            className='hover:scale-125 transition-all duration-500 cursor-pointer'
-                            src='https://i.ibb.co/SmNSVs8/blog-2.png'
-                            alt='plant held by a man'
-                        />
-                        <h2 className='text-xl font-semibold leading-5 mt-8 text-gray-800'>
-                            Busting myths about hair
-                        </h2>
-                        <div className='mt-6 flex items-center cursor-pointer'>
-                            <p className='pr-3 text-base font-medium leading-4 underline text-gray-800'>
-                                5 min read
-                            </p>
-                            <svg
-                                width={16}
-                                height={10}
-                                viewBox='0 0 16 10'
-                                fill='none'
-                                xmlns='http://www.w3.org/2000/svg'
-                            >
-                                <path
-                                    d='M1 5H15'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 9L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 1L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                            </svg>
-                        </div>
-                        <p className='text-base mt-4 italic font-italic leading-4 text-gray-600'>
-                            23, Feburary 2021
-                        </p>
-                    </div>
-                    <div className='lg:mt-10'>
-                        <img
-                            className='hover:scale-125 transition-all duration-500 cursor-pointer'
-                            src='https://i.ibb.co/3pMc5WS/blog-5.png'
-                            alt='lady with plant'
-                        />
-                        <h2 className='text-xl font-semibold leading-5 mt-8 text-gray-800'>
-                            Busting myths about hair
-                        </h2>
-                        <div className='mt-6 flex items-center cursor-pointer'>
-                            <p className='pr-3 text-base font-medium leading-4 underline text-gray-800'>
-                                5 min read
-                            </p>
-                            <svg
-                                width={16}
-                                height={10}
-                                viewBox='0 0 16 10'
-                                fill='none'
-                                xmlns='http://www.w3.org/2000/svg'
-                            >
-                                <path
-                                    d='M1 5H15'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 9L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 1L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                            </svg>
-                        </div>
-                        <p className='text-base mt-4 italic font-italic leading-4 text-gray-600'>
-                            23, Feburary 2021
-                        </p>
-                    </div>
+                    <PostCard
+                        image={'https://picsum.photos/200/300'}
+                        imageAlt={'t'}
+                        header={'t'}
+                        timeRead={'3min'}
+                        postCreatedDate={'t'}
+                    />
+                    <PostCard
+                        image={'https://picsum.photos/200/300'}
+                        imageAlt={'t'}
+                        header={'t'}
+                        timeRead={'3min'}
+                        postCreatedDate={'t'}
+                    />
                 </div>
                 <div className='flex xl:flex-col sm:flex-row flex-col items-start xl:gap-0 gap-6 xl:w-96 w-auto'>
-                    <div>
-                        <img
-                            src='https://i.ibb.co/TLJdmSt/new-img-2.png'
-                            alt='Delighful breakfast'
-                        />
-                        <h2 className='text-xl font-semibold leading-5 mt-8 text-gray-800'>
-                            Busting myths about hair
-                        </h2>
-                        <div className='mt-6 flex items-center cursor-pointer'>
-                            <p className='pr-3 text-base font-medium leading-4 underline text-gray-800'>
-                                5 min read
-                            </p>
-                            <svg
-                                width={16}
-                                height={10}
-                                viewBox='0 0 16 10'
-                                fill='none'
-                                xmlns='http://www.w3.org/2000/svg'
-                            >
-                                <path
-                                    d='M1 5H15'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 9L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 1L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                            </svg>
-                        </div>
-                        <p className='text-base mt-4 italic font-italic leading-4 text-gray-600'>
-                            23, Feburary 2021
-                        </p>
-                    </div>
-                    <div className='xl:mt-10'>
-                        <img
-                            src='https://i.ibb.co/c8dPSYB/blog-6.png'
-                            alt='hats with image frame in middle'
-                        />
-                        <h2 className='text-xl font-semibold leading-5 mt-8 text-gray-800'>
-                            Busting myths about hair
-                        </h2>
-                        <div className='mt-6 flex items-center cursor-pointer'>
-                            <p className='pr-3 text-base font-medium leading-4 underline text-gray-800'>
-                                5 min read
-                            </p>
-                            <svg
-                                width={16}
-                                height={10}
-                                viewBox='0 0 16 10'
-                                fill='none'
-                                xmlns='http://www.w3.org/2000/svg'
-                            >
-                                <path
-                                    d='M1 5H15'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 9L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                                <path
-                                    d='M11 1L15 5'
-                                    stroke='#1F2937'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                />
-                            </svg>
-                        </div>
-                        <p className='text-base mt-4 italic font-italic leading-4 text-gray-600'>
-                            23, Feburary 2021
-                        </p>
-                    </div>
+                    <PostCard
+                        image={'https://picsum.photos/200/300'}
+                        imageAlt={'t'}
+                        header={'t'}
+                        timeRead={'3min'}
+                        postCreatedDate={'t'}
+                    />
+                    <PostCard
+                        image={'https://picsum.photos/200/300'}
+                        imageAlt={'t'}
+                        header={'t'}
+                        timeRead={'3min'}
+                        postCreatedDate={'t'}
+                    />
                 </div>
             </div>
         </div>
---
+++ src/app/components/posts/PostCad.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { MoveRight } from 'lucide-react';
+import Link from 'next/link';
+
+interface IPostCardProps {
+    id?: number | string;
+    image: string;
+    imageAlt: string;
+    header: string;
+    timeRead: string;
+    postCreatedDate: string;
+}
+
+const PostCard: FC<IPostCardProps> = ({
+    id,
+    image,
+    header,
+    timeRead,
+    imageAlt,
+    postCreatedDate,
+}: IPostCardProps) => {
+    return (
+        <div className='hover:scale-125 transition-all duration-500 cursor-pointer lg:mt-10'>
+            {' '}
+            <img src={image} alt={imageAlt} />
+            <h2 className='text-xl font-semibold leading-5 mt-8 text-gray-800'>
+                {header}
+            </h2>
+            <div className='mt-6 flex items-center cursor-pointer'>
+                <p className='pr-3 text-base font-medium leading-4 underline text-gray-800'>
+                    {timeRead}
+                </p>
+                <Link href={`/post/${id}`}>
+                    <MoveRight />
+                </Link>
+            </div>
+            <p className='text-base mt-4 italic font-italic leading-4 text-gray-600'>
+                {postCreatedDate}
+            </p>
+        </div>
+    );
+};
+
+export default PostCard;
