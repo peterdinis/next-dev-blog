98d81b8f6ce7246d26b20c2c9f31f4546ef586de
Peterdinis <peterdinis0@pobox.sk>
2023-12-29 13:22:52 +0100

    feat: Writing modal for creating posts WIP

--- src/app/components/posts/CreatePost.tsx
+++ src/app/components/posts/CreatePost.tsx
@@ -1,34 +1,50 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
 import { FC, useState } from 'react';
+import PostEditor from './PostEditor';
 
 const CreatePost: FC = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <Dialog
-        open={isOpen}
-        onOpenChange={(v) => {
-            if (!v) {
-                setIsOpen(v);
-            }
-        }}
-    >
-        <DialogTrigger
-            onClick={() => {
-                setIsOpen(true);
+            open={isOpen}
+            onOpenChange={(v) => {
+                if (!v) {
+                    setIsOpen(v);
+                }
             }}
-            asChild
         >
-            <div>Upload PDF</div>
-        </DialogTrigger>
+            <DialogTrigger
+                onClick={() => {
+                    setIsOpen(true);
+                }}
+                asChild
+            >
+                <Button variant={'ghost'} size='lg'>
+                    Create new post
+                </Button>
+            </DialogTrigger>
 
-        <DialogContent>
-            abc
-        </DialogContent>
-    </Dialog>
-    )
+            <DialogContent>
+                <h1 className='scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-5xl'>
+                    Add new post
+                </h1>
+                <div className='mt-6'>
+                    <Input type="text" placeholder='Post title' />
+                    <div className='mt-3'>
+                        <PostEditor />
+                    </div>
+                    <div className='mt-3'>
+                    <Input type="text" placeholder='Post tag' />
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
 };
 
 export default CreatePost;
