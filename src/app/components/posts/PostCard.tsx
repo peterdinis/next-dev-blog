caa1230ba3b6543c2c820e6500e81935aae3d561
Peterdinis <peterdinis0@pobox.sk>
2023-12-29 19:33:06 +0100

    feat: Post card update

--- src/app/components/posts/PostCard.tsx
+++ src/app/components/posts/PostCard.tsx
@@ -2,34 +2,38 @@ import { FC } from 'react';
 import { MoveRight } from 'lucide-react';
 import Link from 'next/link';
 import { PostCardProps } from '@/types/postTypes';
-
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 const PostCard: FC<PostCardProps> = ({
     id,
     image,
     header,
     timeRead,
     imageAlt,
-    postCreatedDate,
 }: PostCardProps) => {
     return (
-        <div className='hover:scale-125 transition-all duration-500 cursor-pointer lg:mt-10'>
-            {' '}
-            <img src={image} alt={imageAlt} />
-            <h2 className='text-xl font-semibold leading-5 mt-8 text-gray-800'>
-                {header}
-            </h2>
-            <div className='mt-6 flex items-center cursor-pointer'>
-                <p className='pr-3 text-base font-medium leading-4 underline text-gray-800'>
-                    {timeRead}
-                </p>
-                <Link href={`/post/${id}`}>
-                    <MoveRight />
-                </Link>
-            </div>
-            <p className='text-base mt-4 italic font-italic leading-4 text-gray-600'>
-                {postCreatedDate}
-            </p>
-        </div>
+        <Card>
+            <CardHeader>
+                <img src={image} alt={imageAlt} />
+                <CardTitle>{header}</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <CardDescription>
+                    <p className='text-base font-medium leading-4 underline text-gray-800'>
+                       {timeRead} read{' '}
+                    </p>
+                    <Link className="mt-8" href={`/post/${id}`}>
+                        <Button variant={"ghost"} size={"lg"}>See Detail<MoveRight className='ml-4' />{' '}</Button>
+                    </Link>
+                </CardDescription>
+            </CardContent>
+        </Card>
     );
 };
 
