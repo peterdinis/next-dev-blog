3ad0b783f066a2d63232a7ac7333c7dae9f36e26
Peterdinis <peterdinis0@pobox.sk>
2023-12-29 12:35:12 +0100

    FOO

--- src/app/components/auth/ProfileComponent.tsx
+++ src/app/components/auth/ProfileComponent.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import LogoutBtn from './LogoutBtn';
+import { Button } from '@/components/ui/button';
+import { format } from 'date-fns';
 
 const ProfileComponent: FC = async () => {
     const supabase = createServerComponentClient({ cookies });
@@ -15,12 +17,39 @@ const ProfileComponent: FC = async () => {
         redirect('/login');
     }
 
-    console.log(user);
+    const formattedDate = format(
+        user?.identities![0]?.created_at as unknown as string,
+        'MM/dd/yyyy',
+    );
 
-    return <div>
-        <h2>{user.email}</h2>
-        <LogoutBtn />
-    </div>;
+    return (
+        <div>
+            <div className='max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5'>
+                <img
+                    className='w-32 h-32 rounded-full mx-auto'
+                    src='https://picsum.photos/200'
+                    alt='Profile picture'
+                />
+                <h2 className='text-center text-2xl font-semibold mt-3'>
+                    {user.email}
+                </h2>
+                <div className='flex justify-center mt-5'>
+                    <LogoutBtn />
+                    <Button variant='ghost' size='lg'>
+                        My posts
+                    </Button>
+                    <Button variant={'ghost'} size='lg'>
+                        Create Post
+                    </Button>
+                </div>
+                <div className='mt-5'>
+                    <h3 className='text-xl font-semibold'>
+                        Account created: {formattedDate}
+                    </h3>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ProfileComponent;
