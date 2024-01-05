c5d0dd582432a49bfba7a1fb46564758f1f33b13
Peterdinis <peterdinis0@pobox.sk>
2024-01-05 20:21:21 +0100

    feat: Updating posts WIP

--- src/app/components/posts/AllPostsWrapper.tsx
+++ src/app/components/posts/AllPostsWrapper.tsx
@@ -52,37 +52,88 @@ const AllPostsWrapper: FC = () => {
 
     return (
         <>
-            <Header text='All Posts' />{' '}
+            <Header text='All Posts' />
             <div className='rounded flex items-center w-full p-3 shadow-sm border border-gray-200'>
                 <input
                     type='search'
                     name=''
                     placeholder='search for posts'
                     className='pl-4 text-sm outline-none focus:outline-none bg-transparent'
-                />{' '}
-            </div>{' '}
+                />
+            </div>
             <div className='mt-6'>
-                {' '}
-                <div className='flex-col items-center justify-center  px-4'>
-                    {' '}
+                <div className='container my-12 mx-auto px-4 md:px-12'>
+                    <div className='flex flex-wrap -mx-1 lg:-mx-4'>
+                        <div className='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3'>
+                            <article className='overflow-hidden rounded-lg shadow-lg'>
+                                <a href='#'>
+                                    <img
+                                        alt='Placeholder'
+                                        className='block h-auto w-full'
+                                        src='https://picsum.photos/600/400/?random'
+                                    />
+                                </a>
+
+                                <header className='flex items-center justify-between leading-tight p-2 md:p-4'>
+                                    <h1 className='text-lg'>
+                                        <a
+                                            className='no-underline hover:underline text-black'
+                                            href='#'
+                                        >
+                                            Article Title
+                                        </a>
+                                    </h1>
+                                    <p className='text-grey-darker text-sm'>
+                                        11/1/19
+                                    </p>
+                                </header>
+
+                                <footer className='flex items-center justify-between leading-none p-2 md:p-4'>
+                                    <a
+                                        className='flex items-center no-underline hover:underline text-black'
+                                        href='#'
+                                    >
+                                        <img
+                                            alt='Placeholder'
+                                            className='block rounded-full'
+                                            src='https://picsum.photos/32/32/?random'
+                                        />
+                                        <p className='ml-2 text-sm'>
+                                            Author Name
+                                        </p>
+                                    </a>
+                                    <a
+                                        className='no-underline text-grey-darker hover:text-red-dark'
+                                        href='#'
+                                    >
+                                        <span className='hidden'>Like</span>
+                                        <i className='fa fa-heart'></i>
+                                    </a>
+                                </footer>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+                {/*  <div className='flex-col items-center justify-center  px-4'>
+                    
                     <div className='2xl:container 2xl:mx-auto flex flex-wrap items-start justify-center pt-6 gap-6'>
-                        {' '}
+                        
                         {frontPosts &&
                             loading === false &&
                             frontPosts.map((item: FrontdPostMapping) => (
                                 <div className='flex lg:flex-col sm:flex-row flex-col items-start lg:gap-0 gap-6 lg:w-96 w-auto'>
-                                    {' '}
+                                    
                                     <FrontPostCard
                                         id={item.id}
                                         title={item.title}
                                         tag={item.tag}
-                                    />{' '}
+                                    />
                                 </div>
-                            ))}{' '}
-                    </div>{' '}
-                </div>{' '}
+                            ))}
+                    </div>
+                </div> */}
             </div>
-            <ScrollToTop smooth={true} />{' '}
+            <ScrollToTop smooth={true} />
         </>
     );
 };
